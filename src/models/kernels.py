aff2503808717c69569105e05017a21e7f0fe378
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-11-28 20:03:08 +0100

    rename kernals kernels and format

--- src/models/kernels.py
+++ src/models/kernels.py
@@ -1,5 +1,5 @@
-
 import numpy as np
+
 
 class Kernel:
     def __add__(self, other):
@@ -7,6 +7,7 @@ class Kernel:
 
     def __mul__(self, other):
         return ProductKernel(self, other)
+
 
 class SumKernel(Kernel):
     def __init__(self, kernel1, kernel2):
@@ -16,6 +17,7 @@ class SumKernel(Kernel):
     def compute(self, x1, x2):
         return self.kernel1.compute(x1, x2) + self.kernel2.compute(x1, x2)
 
+
 class ProductKernel(Kernel):
     def __init__(self, kernel1, kernel2):
         self.kernel1 = kernel1
@@ -24,19 +26,22 @@ class ProductKernel(Kernel):
     def compute(self, x1, x2):
         return self.kernel1.compute(x1, x2) * self.kernel2.compute(x1, x2)
 
+
 class GaussianKernel(Kernel):
     def __init__(self, length_scale=1.0):
         self.length_scale = length_scale
 
     def compute(self, x1, x2):
-        return np.exp(-0.5 * np.sum((x1 - x2) ** 2) / self.length_scale ** 2)
+        return np.exp(-0.5 * np.sum((x1 - x2) ** 2) / self.length_scale**2)
+
 
 class RBFKernel(Kernel):
     def __init__(self, length_scale=1.0):
         self.length_scale = length_scale
 
     def compute(self, x1, x2):
-        return np.exp(-0.5 * np.sum((x1 - x2) ** 2) / (2 * self.length_scale ** 2))
+        return np.exp(-0.5 * np.sum((x1 - x2) ** 2) / (2 * self.length_scale**2))
+
 
 class RationalQuadraticKernel(Kernel):
     def __init__(self, alpha=1.0, length_scale=1.0):
@@ -45,4 +50,4 @@ class RationalQuadraticKernel(Kernel):
 
     def compute(self, x1, x2):
         dist = np.sum((x1 - x2) ** 2)
-        return (1 + dist / (2 * self.alpha * self.length_scale ** 2)) ** (-self.alpha)
+        return (1 + dist / (2 * self.alpha * self.length_scale**2)) ** (-self.alpha)
